c5d7c5f5f36d4f96720565973f0b3cec0e743ca5
Daniel-mf-3 <jdmarchena2003@gmail.com>
2026-03-29 15:55:58 -0600

    Inicio del modulo hamming

---
+++ Proyecto/src/design/module_hamming.sv
@@ -0,0 +1,36 @@
+
+module module_hamming (
+
+
+    input logic [3:0] Numero_binario,
+
+    output logic [6:0] hamming
+
+    
+);
+
+logic P1, P2, P3;
+logic D1, D2, D3, D4;
+
+assign D1 = Numero_binario[0];
+assign D2 = Numero_binario[1];
+assign D3 = Numero_binario[2];
+assign D4 = Numero_binario[3];
+
+assign P1 = D1^D2^D4;
+assign P2 = D1^D3^D4;
+assign P3 = D2^D3^D4;
+
+assign hamming[0]=P1;
+assign hamming[1]=P2;
+assign hamming[2]=D1;
+assign hamming[3]=P3;
+assign hamming[4]=D2;
+assign hamming[5]=D3;
+assign hamming[6]=D4;
+ 
+ 
+
+
+
+endmodule
